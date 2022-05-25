964b9146f50e85541d557886899ce621b9f9f230
Roman <federyakinr@gmail.com>
2022-05-25 16:31:46 +0200

    Start films

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 "use strict";
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', "");
+let personalMovesDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('один из просмотренных фильмов ?', ""),
+    b = prompt('на сколько оцените его?', ""),
+    c = prompt('один из просмотренных фильмов?', ""),
+    d = prompt('на сколько оцените его?', "");
+personalMovesDB.movies[a] = b;
+personalMovesDB.movies[c] = d;
+console.log(personalMovesDB);
